8137c89e9c7522cc3bda7734c4e06d9a8ed3470f
Teevint Prak <teevint19@gmail.com>
2022-01-22 13:46:04 -0800

    Added Turret Aiming/Shooting
    Turrets script now aims at enemy that collides with its isTriggerCollision stored and looped for the closest and instantiates a bullet to shoot at it.
    
    The bullet on hit will call the turret that fired it and remove the gameObject its about to destroy for the list of targets.
    
    Bullets on hit adds particles and destroy game objects.

---
+++ Tower Defense + AI/Assets/Scripts/Bullet.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject impactEffect;
+    public Material impactMaterial;
+
+    private Transform target;
+    private Color impactColor;
+
+    private GameObject turret;
+
+    public float speed = 70f;
+
+    public void Seek (Transform _target, Color _color, GameObject _turret)
+	{
+        target = _target;
+        impactColor = _color;
+        turret = _turret;
+	}
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+		{
+            Destroy(gameObject);
+            return; 
+		}
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+		{
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+	{
+        impactMaterial.color = impactColor;
+        GameObject effectInst = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInst, 2f);
+        Turret script = turret.GetComponent<Turret>();
+        script.removeEnemy(target.gameObject);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+	}
+}
--- Tower Defense + AI/Assets/Scripts/Turret.cs
+++ Tower Defense + AI/Assets/Scripts/Turret.cs
@@ -4,15 +4,115 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	
+    private Transform target;
+	private Color color;
+    private List<GameObject> enemies;
+
+	[Header("Attributes")]
+	public float range = 10f;
+	public float fireRate = 1f;
+	private float fireCountDown = 0f;
+
+
+	[Header("UnitySetupFields")]
+	public Transform partToRotate;
+	public float turnSpeed = 10f;
+
+	public GameObject bulletPrefab;
+	public Transform firePoint;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		enemies = new List<GameObject>();
+		InvokeRepeating("UpdateTarget", 0f, 0.25f);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Enemy")
+		{
+			Debug.Log("Enemy Detected: " + other.gameObject.transform.position);
+			enemies.Add(other.gameObject);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Enemy")
+		{
+			Debug.Log("Enemy Exited: " + other.gameObject.transform.position);
+			enemies.Remove(other.gameObject);
+		}
+	}
+
+	public void removeEnemy(GameObject _deadEnemy)
+	{
+		enemies.Remove(_deadEnemy);
+	}
+
+	void UpdateTarget()
+	{
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+		if (enemies.Count <= 0)
+			return;
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+
+		if(nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+			color = nearestEnemy.GetComponent<Renderer>().material.GetColor("_Color");
+		}
+		else
+		{
+			target = null;
+		}
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (target == null)
+			return;
+
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
+		partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+		if(fireCountDown <= 0f)
+		{
+			Shoot();
+			fireCountDown = 1f / fireRate;
+		}
+
+		fireCountDown -= Time.deltaTime;
     }
+
+	public void Shoot()
+	{
+		GameObject bulletInst = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletInst.GetComponent<Bullet>();
+		
+		if(bullet != null)
+		{
+			bullet.Seek(target, color, gameObject);
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, range);
+	}
 }
